d6f533b5211effc2be958ebf526b267a9befaa30
Conor Walsh <conorwalsh0703@gmail.com>
2019-08-05 17:01:45 +0100

    added bootstrap styling to forms

--- conor_creates/settings.py
+++ conor_creates/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    
+    'django_forms_bootstrap'
 ]
 
 MIDDLEWARE = [
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
