b1c6de01b805ad80b86af180dee6b46e1be41e69
Conor Walsh <conorwalsh0703@gmail.com>
2019-08-16 16:08:14 +0100

    fixed user not being added to order bug

--- payments/models.py
+++ payments/models.py
@@ -1,29 +1,30 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from services.models import Service
 
+
 class SingleOrder(models.Model):
-    user = models.ForeignKey(User, default="1", null=False, on_delete=models.DO_NOTHING)
-    name = models.CharField(max_length=50, blank = False)
-    contact_number = models.CharField(max_length = 25, blank = False)
-    email = models.EmailField(max_length=100, blank = False, default = "")
+    user = models.ForeignKey(
+        User, default="1", null=False, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=50, blank=False)
+    contact_number = models.CharField(max_length=25, blank=False)
+    email = models.EmailField(max_length=100, blank=False, default="")
     date = models.DateField()
-    brief = models.TextField(default="", blank = False)
+    brief = models.TextField(default="", blank=False)
 
     def __str__(self):
         return "{0}-{1}-{2}-{3}".format(self.id, self.date, self.name, self.brief)
 
+
 class SingleOrderUpload(models.Model):
     order = models.ForeignKey(SingleOrder, related_name='upload')
-    uploaded_file = models.FileField(upload_to = 'images')
+    uploaded_file = models.FileField(upload_to='images')
 
 
 class SingleOrderLineItem(models.Model):
-    order = models.ForeignKey(SingleOrder, null = False)
-    service = models.ForeignKey(Service, null = False)
-    quantity = models.IntegerField(blank = False)
+    order = models.ForeignKey(SingleOrder, null=False)
+    service = models.ForeignKey(Service, null=False)
+    quantity = models.IntegerField(blank=False)
 
     def __str__(self):
         return "{0} {1} @ {2}".format(self.quantity, self.service.name, self.service.price)
-    
--- services/views.py
+++ services/views.py
@@ -1,42 +1,55 @@
 import random
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect, reverse
 from payments.forms import SinglePayForm, SingleOrderForm
-from payments.models import SingleOrderLineItem
+from payments.models import SingleOrderLineItem, SingleOrder
 from .models import Service, ServiceImage
 from django.contrib import messages
 from django.conf import settings
 from django.utils import timezone
 import stripe
 
+
 def all_services(request):
     services = Service.objects.all()
-    service_images = services.all() 
- 
+    service_images = services.all()
+
     return render(request, 'services.html', {'services': services, 'images': service_images})
+
 
 def single_service(request, pk):
     service = get_object_or_404(Service, pk=pk)
-    service_images = service.images.all() 
-    
+    service_images = service.images.all()
+
     if request.method == "POST":
         order_form = SingleOrderForm(request.POST)
         payment_form = SinglePayForm(request.POST)
-        
 
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
+            order.user = request.user
             order.save()
             quantity = 1
             serviceID = service.id
 
+            # single_order = SingleOrder(
+            #     user=request.user
+            #     name=
+            #     contact_number=models.CharField(max_length=25, blank=False)
+            #     email=models.EmailField(
+            #         max_length=100, blank=False, default="")
+            #     date=models.DateField()
+            #     brief=models.TextField(default="", blank=False)
+            # )
+
+            # single_order.save()
             service = get_object_or_404(Service, pk=serviceID)
             total = service.price
             order_line_item = SingleOrderLineItem(
-                    order=order,
-                    service=service,
-                    quantity=quantity
-                )
+                order=order,
+                service=service,
+                quantity=quantity
+            )
 
             order_line_item.save()
 
@@ -51,15 +64,16 @@ def single_service(request, pk):
             except stripe.error.CardError:
                 messages.error(
                     request, "The payment has not been processed, please try again.")
-            
+
             if customer.paid:
-                messages.error(request, "Thanks for your payment. We will be in touch soon!")
-                
+                messages.error(
+                    request, "Thanks for your payment. We will be in touch soon!")
 
                 return redirect(reverse('index'))
 
             else:
-                messages.error(request, "We were unable to process the transcation, please try again.")
+                messages.error(
+                    request, "We were unable to process the transcation, please try again.")
 
         else:
             print(payment_form.errors)
