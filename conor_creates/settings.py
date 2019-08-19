5811d9ebf230f72db4d5cc8352e69bd933884946
Conor Walsh <conorwalsh0703@gmail.com>
2019-08-19 15:42:16 +0100

    fixed typo in settings

--- conor_creates/settings.py
+++ conor_creates/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'conor_creates.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-if "DATABASE.URL" in os.environ:
+if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
