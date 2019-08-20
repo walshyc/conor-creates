3742474549f542738d48e77d2d0acaa12ad05963
Conor Walsh <conorwalsh0703@gmail.com>
2019-08-20 19:40:51 +0100

    fixed indent in settings

--- conor_creates/settings.py
+++ conor_creates/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'conor_creates.wsgi.application'
 
 
 
- if "DATABASE_URL" in os.environ:
+if "DATABASE_URL" in os.environ:
      DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
- else:
+else:
      print("Database URL not found. Using SQLite instead")
      DATABASES = {
          'default': {
